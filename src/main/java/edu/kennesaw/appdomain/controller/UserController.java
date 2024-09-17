35aaa736d03363325914f372fd54e23680f103f3
BuildTools <unconfigured@null.spigotmc.org>
2024-09-17 01:52:19 -0400

    Supports HTTP Status for Login

--- src/main/java/edu/kennesaw/appdomain/controller/UserController.java
+++ src/main/java/edu/kennesaw/appdomain/controller/UserController.java
@@ -3,6 +3,8 @@ package edu.kennesaw.appdomain.controller;
 import edu.kennesaw.appdomain.User;
 import edu.kennesaw.appdomain.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "http://15.204.199.108")
@@ -20,11 +22,11 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public String loginUser(@RequestBody User user) {
+    public ResponseEntity<User> loginUser(@RequestBody User user) {
         User loggedInUser = userService.loginUser(user.getUsername(), user.getPassword());
         if (loggedInUser != null) {
-            return "Login Successful!";
+            return ResponseEntity.ok(loggedInUser);
         }
-        return "Login Failed!";
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 }
