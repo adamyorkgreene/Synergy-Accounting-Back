569c8d11c88dfc23b92cf5cacc911129f1314f56
BuildTools <unconfigured@null.spigotmc.org>
2024-09-17 08:37:00 -0400

    Additional registration security features

---
+++ src/main/java/edu/kennesaw/appdomain/dto/RegistrationRequest.java
@@ -0,0 +1,42 @@
+package edu.kennesaw.appdomain.dto;
+
+public class RegistrationRequest {
+
+    private String email;
+    private String username;
+    private String password;
+    private String confpassword;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfpassword() {
+        return confpassword;
+    }
+
+    public void setConfpassword(String confpassword) {
+        this.confpassword = confpassword;
+    }
+
+}
